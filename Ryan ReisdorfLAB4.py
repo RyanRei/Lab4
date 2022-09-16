d276b8413fc527efb98e062f64315a5f83b69422
RyanRei <112183249+RyanRei@users.noreply.github.com>
2022-09-16 09:30:16 -0500

    Add files via upload

---
+++ Ryan ReisdorfLAB4.py
@@ -0,0 +1,23 @@
+for count in range(0,100):
+	count+=1
+	num=count**2
+	a=num%10
+	if a==4:
+		print(num)	
+
+for value in range(1):
+	nmbr=list(map(int,input("Enter 5 positive numbers: ").split()))
+	largestNmbr= max(nmbr)
+	print(largestNmbr,"is the largest number")
+	
+import random
+x=random.randint(5,10)
+for i in range(x):
+	print("Hello")
+	
+sentence=input("Enter sentence: ")
+for letter in sentence:
+	vowel=='a' or 'e' or 'i' or 'o' or 'u'
+	vowel=''
+	print(vowel)
+	
